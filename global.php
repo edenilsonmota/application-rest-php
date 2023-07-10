f628533b4b87e9705d846fb13548a1e35e012bec
Edenilson Mota <edenilsonmota.job@gmail.com>
2023-07-10 20:41:36 -0300

    global.php

---
+++ global.php
@@ -0,0 +1,20 @@
+<?php
+
+//info errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ERROR);
+
+define("HOST", 'localhost');
+define("BANCO", 'apiREST');
+define("USER", 'esm');
+define("PASS", 'teste123');
+
+define("DS", DIRECTORY_SEPARATOR); //separador de diretorio
+define("DIR_APP", __DIR__); //raiz projeto
+
+if(file_exists('autoload.php')){
+    include 'autoload.php';
+}else{
+    echo "Erro ao incluir variaveis globais";exit;
+}
