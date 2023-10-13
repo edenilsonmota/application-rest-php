6c5251cb9d910d6b56c5bcaa900f6fc19f37cba4
Edenilson Mota <edenilsonmota.job@gmail.com>
2023-10-13 01:17:29 -0300

    Create method processarRequest and direcionarRequest

--- Classes/Validator/RequestValidator.php
+++ Classes/Validator/RequestValidator.php
@@ -2,9 +2,18 @@
 
 namespace Validator;
 
+use Generator;
+use Util\ConstantesGenericasUtil;
+use Util\JsonUtil;
+
 class RequestValidator
 {
     private $request;
+    private $dadosRequest = [];
+
+    const GET = 'GET';
+    const DELETE = 'DELETE';
+
 
     public function __construct($request)
     {
@@ -12,9 +21,35 @@ class RequestValidator
     }
 
 
+    /**
+     *  function
+     *
+     * @return String
+     */
     public function processarRequest()
     {
+        $retorno = ConstantesGenericasUtil::MSG_ERRO_TIPO_ROTA;
+
+        $this->request['metodo'] == 'POST';
+
+        if(in_array($this->request['metodo'],ConstantesGenericasUtil::TIPO_REQUEST, true)){
+            $retorno = $this->direcionarRequest();
+        }
         
+
+        return $retorno;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    private function direcionarRequest()
+    {
+        if ($this->request['metodo'] !== self::GET && $this->request['metodo'] !== self::DELETE){
+            $this->dadosRequest = JsonUtil::tratarCorpoRequisicaoJson();
+        }
     }
 
 }
